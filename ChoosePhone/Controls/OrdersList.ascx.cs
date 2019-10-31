07e707f88dc354f109f6ec868c6d628cb4337ead
anhvupt <anhvupt2412@gmail.com>
2019-10-31 15:40:30 +0700

    admin orderlist manager

--- ChoosePhone/Controls/OrdersList.ascx.cs
+++ ChoosePhone/Controls/OrdersList.ascx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using DataAccess.DTO;
 
 namespace ChoosePhone.Controls
 {
@@ -13,8 +14,23 @@ namespace ChoosePhone.Controls
         {
             if (!IsPostBack)
             {
-
+                PopulateControls();
             }
         }
+        private void PopulateControls()
+        {
+            DataAccess.DTO.Order sample = new DataAccess.DTO.Order();
+            sample.OrderID = 1;
+            sample.ProductID = 1;
+            sample.CustomerID = 1;
+            List<DataAccess.DTO.Order> orders = new List<DataAccess.DTO.Order>();
+            for (int i = 0; i < 5; i++)
+                orders.Add(sample);
+
+            dlOrdersList.DataSource = orders;
+            dlOrdersList.DataBind();
+
+        }
+
     }
 }
